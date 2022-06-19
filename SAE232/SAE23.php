4fb5be87a44d3c9bd041d753e4d98ef981a15af5
louistudor <105842011+louistudor@users.noreply.github.com>
2022-06-19 16:53:07 +0200

    Update SAE23.php

--- SAE232/SAE23.php
+++ SAE232/SAE23.php
@@ -1,5 +1,5 @@
 <?php
-/* Script de connexion à la base smi */
+/* Connexion scrypt to connect to data base */
 
   $id_bd = mysqli_connect("localhost","root","passroot","SAE23")
     or die("Connexion au serveur et/ou à la base de données impossible");
